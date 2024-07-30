25d88b296cfca4d9c0b6b8af210c87a9ac362177
Ratnesh Tiwari <ratnesh.tiwari@karexpert.com>
2024-07-30 17:56:32 +0530

    add

---
+++ src/main/java/com/csi/git/core/java/GitCoreJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.csi.git.core.java;
+
+/**
+ *
+ * @author ratnesh
+ */
+public class GitCoreJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git Core Java World...!!!");
+    }
+}
